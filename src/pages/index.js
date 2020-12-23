daf6f71e7a6adf1cb0b4cf5b30f7cca025102f49
Alisha Evans <alishaevn2@gmail.com>
2020-12-22 22:48:26 -0800

    using graphql hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 const App = () => {
-  return <h1>Hello World!</h1>
+	const { title, description } = useSiteMetadata()
+	return (
+		<>
+		  <h1>{title}</h1>
+		  <p>{description}</p>
+		</>
+	  )
 }
 
 export default App
