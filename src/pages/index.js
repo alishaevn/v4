ec58aa01806e15f0dc34cf4c11cff3907752d6b5
Alisha Evans <alishaevn2@gmail.com>
2020-12-25 00:13:26 -0800

    more blog setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,45 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 
-const App = () => (
-	<Layout />
+const App = ({ data }) => (
+	<Layout>
+		{data.allMdx.nodes.map(({ excerpt, fields, frontmatter, id }) => (
+			<div key={id}>
+				<Link to={fields.slug}>
+					<h1>{frontmatter.title}</h1>
+					<p>{frontmatter.date}</p>
+					<p>{excerpt}</p>
+				</Link>
+			</div>
+        ))}
+	</Layout>
 )
 
 export default App
+
+export const query = graphql`
+	query SITE_INDEX_QUERY {
+		allMdx(
+			sort: {
+				fields: [ frontmatter___date ],
+				order: DESC,
+			},
+			filter: {
+				frontmatter: {
+					published: { eq: true },
+				},
+			},
+		) {
+			nodes {
+				id
+				excerpt(pruneLength: 250)
+				frontmatter {
+					title
+					date
+				}
+				fields { slug }
+			}
+		}
+	}
+`
