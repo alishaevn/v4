2dc5cf0f9d13531b39804b91131c1fd75bf352b2
Alisha Evans <alishaevn2@gmail.com>
2021-01-05 20:58:20 -0800

    created the blog post template

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,9 +1,30 @@
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import React from 'react'
+import Layout from '../components/Layout'
 
-export default () => {
-  return (
-    <>
-      <p>post here</p>
-    </>
-  )
+const Post = ({ data }) => {
+	const { frontmatter, body } = data.mdx
+
+	return (
+		<Layout>
+			<h1>{frontmatter.title}</h1>
+			<p>{frontmatter.date}</p>
+			<MDXRenderer>{body}</MDXRenderer>
+		</Layout>
+	)
 }
+
+export default Post
+
+export const query = graphql`
+	query PostByPath($path: String!) {
+		mdx(fields: {slug: {eq: $path}}) {
+			body
+			frontmatter {
+				title
+				date(formatString: "YYYY MMMM Do")
+			}
+		}
+	}
+`
