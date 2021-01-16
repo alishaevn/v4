37799ccefa5d54e7cb4c4c7937a91c30163fbbb5
Alisha Evans <alishaevn2@gmail.com>
2021-01-16 02:09:36 -0800

    begin to set up nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,22 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
+
+const PostWrapper = styled.nav`
+	display: flex;
+	justify-content: space-between;
+`
 
 const Header = ({ siteTitle, siteDescription }) => (
-	<div>
+	<PostWrapper>
 		<Link to='/'>
-			<h1>{siteTitle}</h1>
+			<h1>Home</h1>
 		</Link>
 		<Link to='/blog'>
 			<h1>Blog</h1>
 		</Link>
-		<p>{siteDescription}</p>
-	</div>
+		{/* <p>{siteDescription}</p> */}
+	</PostWrapper>
 )
 
 export default Header
