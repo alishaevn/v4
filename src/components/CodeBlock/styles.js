8e0e8192efaf9165a1e3bc8a1914635fad686a3d
Alisha Evans <alishaevn2@gmail.com>
2021-01-16 23:09:21 -0800

    begin setting up media queries for code blocks

--- src/components/CodeBlock/styles.js
+++ src/components/CodeBlock/styles.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const Pre = styled.pre`
-	text-align: left;
-	margin: 1em 0;
 	padding: 0.5em;
 	overflow-x: auto;
-	border-radius: 3px;
+	border-radius: 5px;
+	max-width: 280px;
 
 	& .token-line {
 		line-height: 1.3em;
