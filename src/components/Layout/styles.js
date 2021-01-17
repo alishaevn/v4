8002af73f94b80f9032761fb78a20eb0f7a0cb4a
Alisha Evans <alishaevn2@gmail.com>
2021-01-16 20:26:50 -0800

    setup media queries

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,6 +1,27 @@
 import styled from 'styled-components'
+import { devices } from '../../utilities/devices'
 
 export const AppStyles = styled.main`
-	width: 800px;
+	width: 320px;
 	margin: 0 auto;
+
+	@media ${devices.mobileM} { 
+		width: 345px;
+	}
+
+	@media ${devices.mobileL} { 
+		width: 405px;
+	}
+
+	@media ${devices.tablet} { 
+		width: 718px;
+	}
+
+	@media ${devices.laptop} { 
+		width: 800px;
+	}
+	
+	@media ${devices.desktop} {
+		max-width: 1400px;
+	}
 `
