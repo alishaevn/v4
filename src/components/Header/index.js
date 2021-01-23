1db002432ec6ca686960fe48ff5119d1ddb48153
Alisha Evans <alishaevn2@gmail.com>
2021-01-22 21:31:26 -0800

    added the logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,23 @@
 import { Link } from 'gatsby'
 import React from 'react'
-import { H1, PostWrapper } from './styles'
+import { H1, Logo, NavBar, NavLinks } from './styles'
+import logo from '../../../assets/images/seafoam_horizontal.png'
 
 const Header = () => (
-	<PostWrapper>
-		<Link to='/'>
-			<H1>home</H1>
-		</Link>
-		<Link to='/about'>
-			<H1>about</H1>
-		</Link>
-		<Link to='/posts'>
-			<H1>posts</H1>
-		</Link>
-	</PostWrapper>
+	<NavBar>
+		<Logo src={logo} />
+		<NavLinks>
+			<Link to='/'>
+				<H1>home</H1>
+			</Link>
+			<Link to='/about'>
+				<H1>about</H1>
+			</Link>
+			<Link to='/posts'>
+				<H1>posts</H1>
+			</Link>
+		</NavLinks>
+	</NavBar>
 )
 
 export default Header
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -6,10 +6,15 @@ export const H1 = styled.h1`
 	color: ${golden};
 `
 
-export const PostWrapper = styled.nav`
-	// @media ${devices.laptop} { 
-		display: flex;
-		justify-content: space-between;
-		width: 200px;
-	// }
+export const Logo = styled.img`
+	height: 35px;
+`
+
+export const NavBar = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	width: 200px;
+`
+
+export const NavLinks = styled.div`
 `
