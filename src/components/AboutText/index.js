33bdd413b31a4d65603d1c9fb7b29251bcc9f0fe
Alisha Evans <alishaevn2@gmail.com>
2021-01-28 00:11:47 -0800

    add some about text

---
+++ src/components/AboutText/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from './styles'
+
+const AboutText = () => (
+	<>
+		<Text>
+			I’m currently a full stack software developer for Notch8, a web and mobile applications consultancy in San Diego, CA. I primarily build iOS and android apps with React Native, Redux, MobX and Ruby on Rails. I love to bring design mock-ups to life by writing simple, efficient code that adheres to best practices.
+		</Text>
+		<Text>
+			I have a B.A. in Graphic Design with a minor in Marketing and an M.F.A. in Media Design. I’ve designed and executed visual brand identities, including logos, print collateral, style guides and user interfaces for individuals and companies. I’ve also photographed, culled and edited headshots used in professional publications.
+		</Text>
+		<Text>
+			As a student-athlete for a large part of my life, I understand and appreciate the value of communication and teamwork. I have also coached girl’s high school and club volleyball to pass those lessons on. My hobbies include watching basketball and football, building puzzles, writing poetry and photography.
+		</Text>
+	</>
+)
+
+export default AboutText
---
+++ src/components/AboutText/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { devices } from '../../utilities/devices'
+import { golden, white } from '../../utilities/colors'
+import '../../utilities/typography.scss'
+
+export const Text = styled.p`
+	color: ${white};
+	font-family: 'provicali';
+	font-size: 14px;
+	line-height: 18px;
+	letter-spacing: 1px;
+`
