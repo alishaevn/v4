900477841401cc961e3715681a604a5219bdd1e3
Alisha Evans <alishaevn2@gmail.com>
2021-01-30 23:55:15 -0800

    add the profile pic to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import SEO from 'react-seo-component'
 import AboutText from '../components/AboutText'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
+import ProfilePic from '../components/ProfilePic'
 import useSiteMetadata from '../hooks/useSiteMetadata'
 
 const About = () => {
@@ -26,6 +27,10 @@ const About = () => {
 				siteLanguage={siteLanguage}
 				siteLocale={siteLocale}
 				twitterUsername={twitterUsername}
+			/>
+			<ProfilePic
+				marginBottom={50}
+				marginTop={50}
 			/>
 			<Header title='about me' />
 			<AboutText />
