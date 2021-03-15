4c9ad34d87263dbc08daa0c740633492bcd93758
Alisha Evans <alishaevn2@gmail.com>
2021-03-14 22:20:14 -0700

    move all of the headers into a single file

---
+++ src/components/markdown-styles/Headers.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { seaFoam, white } from '../../utilities/colors'
+import { devices } from '../../utilities/devices'
+
+export const Header1 = styled.h1`
+    font-family: 'quicksand';
+    color: ${seaFoam};
+    letter-spacing: 0.5px;
+    font-size: 40px;
+`
+
+export const Header2 = styled.h2`
+    font-family: 'montserrat';
+    color: ${white};
+    text-transform: uppercase;
+
+    @media ${devices.laptop} {
+        font-size: 30px;
+    }
+`
+
+export const Header3 = styled.h3`
+    font-family: 'montserrat';
+    color: ${white};
+    text-transform: uppercase;
+`
+
+/*
+    Resources:
+    - https://github.com/giteden/mdx-gatsby-demo
+ */
