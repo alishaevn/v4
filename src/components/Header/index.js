7cccda1009b06e81a7373f963ba91fde2a598ada
alishaevn <alieva.inc@gmail.com>
2021-04-06 21:59:55 -0700

    edit wording in the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = ({ details, title }) => {
 	return (
 		<>
 			<Title>{title}</Title>
-			{details && <Details>Just a bit of what I’ve learned along the way</Details>}
+			{details && <Details>A bit of what I’ve learned along the way</Details>}
 		</>
 	)
 }
