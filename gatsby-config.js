fa197d693cb51bf3915418bc21cfa6de9c219f8f
alishaevn <alieva.inc@gmail.com>
2021-04-08 22:33:03 -0700

    wip -- get emojis to work in mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
 const { config } = require('./content/meta/config')
+// const emoji = require('remark-emoji')
+// import emoji from 'remark-emoji'
 
 module.exports = {
     siteMetadata: config,
@@ -11,7 +13,9 @@ module.exports = {
             resolve: 'gatsby-plugin-mdx',
             options: {
                 extensions: ['.mdx', '.md'],
+                // remarkPlugins: [emoji],
                 gatsbyRemarkPlugins: [
+                    // emoji,
                     {
                         resolve: 'gatsby-remark-images',
                         options: { maxWidth: 590 },
@@ -35,6 +39,27 @@ module.exports = {
                 rule: {
                     include: /svg/
                 }
+            }
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [{
+                    resolve: 'gatsby-remark-emojis',
+                    options: {
+                        active: true, // Deactivate the plugin globally (default: true)
+                        class: 'emoji-icon', // Add a custom css class
+                        size: 16, // Select the size (available size: 16, 24, 32, 64)
+                        styles: { // Add custom styles
+                            display: 'inline',
+                            margin: '0',
+                            'margin-top' : '1px',
+                            position: 'relative',
+                            top: '5px',
+                            width: '25px',
+                        },
+                    }
+                }]
             }
         }
     ],
