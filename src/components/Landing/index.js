c737a12b35ccb551b35587d0e8183aa315b94f74
Alisha Evans <alisha@notch8.com>
2021-04-16 14:01:42 -0700

    replace the static poly on the homepage with an animated one

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useEffect, createRef } from 'react'
+import lottie from 'lottie-web'
 import { H1, Welcome, Wrapper, poly } from './styles'
-import PinkPoly from '../../../assets/svg/pink-polygon.svg'
+import pinkPoly from '../../../assets/animations/pink-poly.json'
 
-const Landing = () => (
-	<Wrapper>
-		<H1>hello</H1>
-		<Welcome>
-			{`my name is alisha,`}<br></br>
-			{`a software developer with a love for making aesthetically dope web and mobile apps.`}<br></br>
-			{`thanks for stopping by.`}
+const Landing = () => {
+	const animationContainer = createRef()
+
+	useEffect(() => {
+		const anim = lottie.loadAnimation({
+			container: animationContainer.current,
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			animationData: pinkPoly
+		});
+		return () => anim.destroy() // optional clean up for unmounting
+	}, [])
+
+	lottie.loadAnimation({
+		container: animationContainer.current, // current instance of our container!
+		animationData: pinkPoly, // animation file!
+		renderer: 'svg',
+		loop: true,
+		autoplay: true
+	})
+
+	return (
+		<Wrapper>
+			<H1>hello</H1>
+			<Welcome>
+				{`my name is alisha,`}<br></br>
+				{`a software developer with a love for making aesthetically dope web and mobile apps.`}<br></br>
+				{`thanks for stopping by.`}
 			</Welcome >
 			<div style={poly}>
-				<PinkPoly />
+				<div ref={animationContainer} />
 			</div>
-	</Wrapper>
-)
+		</Wrapper>
+	)
+}
 
 export default Landing
