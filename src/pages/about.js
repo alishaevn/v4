b0ff84172fdbbce4adeffdb9a24566e926a50a39
Alisha Evans <alisha@notch8.com>
2021-04-26 00:05:58 -0700

    cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import SEO from 'react-seo-component'
-import styled from 'styled-components'
-import AboutText from '../components/AboutText'
+import AboutBody from '../components/AboutBody'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
 import useSiteMetadata from '../hooks/useSiteMetadata'
-import { devices } from '../utilities/devices'
-import profile from '../../assets/images/profile-pic.png'
+
 
 const About = () => {
 	const {
@@ -30,28 +28,10 @@ const About = () => {
 				siteLocale={siteLocale}
 				twitterUsername={twitterUsername}
 			/>
-			<ProfilePic src={profile} />
 			<Header title='about me' />
-			<AboutText />
+			<AboutBody />
 		</Layout>
 	)
 }
 
 export default About
-
-const ProfilePic = styled.img`
-	width: 100%;
-	margin-bottom: 25px;
-	margin-top: 25px;
-	display: block;
-	object-fit: cover;
-	height: 175px;
-	object-position: 20% 25%;
-
-	@media ${devices.tablet} {
-		height: 175px;
-		width: 175px;
-		margin-top: 110px;
-		margin-bottom: 110px;
-	}
-`
